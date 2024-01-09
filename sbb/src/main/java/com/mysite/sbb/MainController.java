1f2402223f70d2cff914c7227f1898219cf4efd3
pahkey <pahkey@gmail.com>
2024-01-09 15:23:26 +0900

    2-08

--- sbb/src/main/java/com/mysite/sbb/MainController.java
+++ sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -10,6 +10,11 @@ public class MainController {
 	@GetMapping("/sbb")
 	@ResponseBody
 	public String index() {
-		return "안녕하세요 sbb에 오신 것을 환영합니다.";
+		return "안녕하세요 sbb에 오신것을 환영합니다.";
+	}
+
+	@GetMapping("/")
+	public String root() {
+		return "redirect:/question/list";
 	}
 }
